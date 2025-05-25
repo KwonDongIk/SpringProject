53c8297c01752e346d0c7bffbea9d18e527604c7
KwonDongIk <dongikik@kakao.com>
2025-05-25 19:59:03 +0900

    0525 PM 19:59 commit

--- di/src/main/java/com/yedam/app/di/App.java
+++ di/src/main/java/com/yedam/app/di/App.java
@@ -1,7 +1,6 @@
 package com.yedam.app.di;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -12,8 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-    	ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfigure.class);
-    	//ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+    	//ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfigure.class);
+    	ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
     	// 상속과 다형성
     	// 같은 코드지만 참조하는 대상에 따라서 실행 결과값이 다르다.
     	// LgTV tv = new LgTV();
--- di/src/main/java/com/yedam/app/di/SamsungTV.java
+++ di/src/main/java/com/yedam/app/di/SamsungTV.java
@@ -2,11 +2,15 @@ package com.yedam.app.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-@Component("tv")
+//@Component("tv")
 public class SamsungTV implements TV{
 	
 	//@Autowired
 	private Speaker speaker;
+
+	public void setSpeaker(Speaker speaker) {
+		this.speaker = speaker;
+	}
 
 	public void powerOn() {
 		System.out.println("SamsungTV powerOn");
